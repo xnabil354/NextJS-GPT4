1bd5be80175898722839e5cc7666f883df154c01
nabil354 <nabil.voel354@gmail.com>
2023-03-19 11:41:49 +0700

    Fixed Update

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -1,20 +1,18 @@
-// Make sure to add OPENAI_API_KEY as a secret
-
 import { Configuration, OpenAIApi } from "openai";
 
+require('dotenv').config()
+
 const configuration = new Configuration({
-  apiKey: "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",
+  apiKey: process.env.OPENAI_APIKEY,
 });
 
 const openai = new OpenAIApi(configuration);
 
 export default async function(req, res) {
   const completion = await openai.createChatCompletion({
-    // You need early access to GPT-4, otherwise use "gpt-3.5-turbo"
     model: "gpt-3.5-turbo",
+    // messages: [{role: "user", content: "Hello world"}],
     messages: [{ "role": "system", "content": "You are a helpful assistant." }].concat(req.body.messages)
-
   });
   res.status(200).json({ result: completion.data.choices[0].message })
-
 }
